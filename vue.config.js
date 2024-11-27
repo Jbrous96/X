08f64cfbfb9f9c9188ad4a7f86efb59a32de941a
Jacob Broussard <53851408+Jbrous96@users.noreply.github.com>
2024-11-27 15:47:33 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',
+  publicPath: './', 
   outputDir: 'dist',
   chainWebpack: config => {
-    config.optimization.minimize(false)
     config.optimization.minimizer('terser').tap((args) => {
-      const opts = args[0]
+      const opts = args[0];
       opts.terserOptions = {
         ...opts.terserOptions,
         keep_classnames: true,
         keep_fnames: true
-      }
-      return args
-    })
+      };
+      return args;
+    });
   },
-  productionSourceMap: false,
+  productionSourceMap: false, 
   css: {
-    sourceMap: false
+    sourceMap: false 
   }
-})
+});
