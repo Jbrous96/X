b30e56de0afd69dd5fd2c371eb3663874aa2cb3b
Jacob Broussard <53851408+Jbrous96@users.noreply.github.com>
2024-12-08 04:29:45 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/main.css'
-
+console.log('App starting...');
+createApp(App).mount('#app');
+console.log('App mounted');
 // Font Awesome Setup
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
