6cd2cd98f31cf0851ac92f2253b5f3f88a8e1798
Jacob Broussard <53851408+Jbrous96@users.noreply.github.com>
2024-12-08 04:32:06 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    react({
-      include: /\.(jsx|tsx|js)$/,
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': '/src',
       'three': 'three'
     }
   },
-  optimizeDeps: {
-    include: ['three', '@react-three/fiber', '@react-three/drei', 'react', 'react-dom']
-  }
 })
